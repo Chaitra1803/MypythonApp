5d0d7df50be82190ef1d9467a069bf998492eb57
Chaitra Teggi <48706140+Chaitra1803@users.noreply.github.com>
2024-01-15 12:24:04 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -4,7 +4,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Welcome to Devops and CICD'
+  return f'''Welcome to Devops and CICD!
+
+Embrace the seamless integration of development and operations for efficient software delivery.
+Unlock the power of automation to streamline your workflows and achieve faster time to market.
+Build a culture of continuous improvement and innovation through effective CI/CD practices.
+Let's embark on a journey of agility and resilience in software development together!'''
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+  app.run(debug=True, host='0.0.0.0')
